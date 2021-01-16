23a34b2ce14ab6e3aaf82bda9b7c08685007e824
tong triwanapong <77496545+ktriwanapong@users.noreply.github.com>
2021-01-16 17:26:19 +0700

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,122 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function() {
+    // Load dinos.
+    $.getJSON('../dinos.json', function(data) {
+      var dinosRow = $('#dinosRow');
+      var dinoTemplate = $('#dinoTemplate');
+
+      for (i = 0; i < data.length; i ++) {
+        dinoTemplate.find('.panel-title').text(data[i].name);
+        dinoTemplate.find('img').attr('src', data[i].picture);
+        dinoTemplate.find('.dino-species').text(data[i].species);
+        dinoTemplate.find('.dino-age').text(data[i].age);
+        dinoTemplate.find('.dino-hometown').text(data[i].hometown);
+        dinoTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        dinosRow.append(dinoTemplate.html());
+      }
+    });
+
+    return await App.initWeb3();
+  },
+
+  
+  initWeb3: async function() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('DinoSnatch.json', function (data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var DinoSnatchArtifact = data;
+      App.contracts.DinoSnatch = TruffleContract(DinoSnatchArtifact);
+
+      // Set the provider for our contract
+      App.contracts.DinoSnatch.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted dinos
+      return App.markAdopted();
+    });
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function() {
+    var DinoSnatchInstance;
+
+    App.contracts.DinoSnatch.deployed().then(function (instance) {
+      DinoSnatchInstance = instance;
+
+      return DinoSnatchInstance.getdinos.call();
+    }).then(function (dinos) {
+      for (i = 0; i < dinos.length; i++) {
+        if (dinos[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-dino').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function (err) {
+      console.log(err.message);
+    });
+  },
+
+  handleAdopt: function(event) {
+    event.preventDefault();
+
+    var dinoId = parseInt($(event.target).data('id'));
+
+    var DinoSnatchInstance;
+
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.DinoSnatch.deployed().then(function (instance) {
+        DinoSnatchInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        return DinoSnatchInstance.adopt(dinoId, { from: account });
+      }).then(function (result) {
+        return App.markAdopted();
+      }).catch(function (err) {
+        console.log(err.message);
+      });
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
